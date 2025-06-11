79bffac8c873c19bf9db3f1e32dc90064c334fff
Ajeet Kumar Yadav <58228449+Ajeet-kumar1@users.noreply.github.com>
2025-06-11 18:21:19 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,25 @@
+import net
+import torch
+import numpy as np
+
+
+
+adaface_models = {
+    'ir_50':"pretrained/adaface_ir50_ms1mv2.ckpt",
+}
+
+def load_pretrained_model(architecture='ir_50'):
+    # load model and pretrained statedict
+    assert architecture in adaface_models.keys()
+    model = net.build_model(architecture)
+    statedict = torch.load(adaface_models[architecture])['state_dict']
+    model_statedict = {key[6:]:val for key, val in statedict.items() if key.startswith('model.')}
+    model.load_state_dict(model_statedict)
+    model.eval()
+    return model
+
+def to_input(pil_rgb_image):
+    np_img = np.array(pil_rgb_image)
+    brg_img = ((np_img[:,:,::-1] / 255.) - 0.5) / 0.5
+    tensor = torch.tensor([brg_img.transpose(2,0,1)]).float()
+    return tensor
